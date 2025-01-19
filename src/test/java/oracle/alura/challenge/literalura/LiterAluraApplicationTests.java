544b0ebe3cfb00e6a8cc878b323aa19abbf4aa3e
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2025-01-19 03:55:32 -0300

    creado el proyecto inicial

---
+++ src/test/java/oracle/alura/challenge/literalura/LiterAluraApplicationTests.java
@@ -0,0 +1,12 @@
+package oracle.alura.challenge.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
